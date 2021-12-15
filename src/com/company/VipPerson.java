2a633c25cfc3d0847e4261623e802dd699d28d74
RadeS7 <rades7@gmail.com>
2021-12-15 20:09:06 +0100

    added another class

---
+++ src/com/company/VipPerson.java
@@ -0,0 +1,32 @@
+package com.company;
+
+public class VipPerson {
+    private String name;
+    private double creditLimit;
+    private String emailAdress;
+
+    public VipPerson (){
+        this ("Default name", 50000.00, "default@email.com");
+    }
+    public VipPerson(String name, double creditLimit) {
+        this (name, creditLimit, "unknown@email.com");
+    }
+
+    public VipPerson(String name, double creditLimit, String emailAdress) {
+        this.name = name;
+        this.creditLimit = creditLimit;
+        this.emailAdress = emailAdress;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getCreditLimit() {
+        return creditLimit;
+    }
+
+    public String getEmailAdress() {
+        return emailAdress;
+    }
+}
